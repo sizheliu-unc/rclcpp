3aa131315a929f02424d2a0c40357e10a4481d19
Sizhe Liu <sizheliu@cs.unc.edu>
2024-09-26 21:04:36 -0400

    Initial change for executors with multi-threading >Summary: 1. Changed the main functionality of rclcpp::executors::SingleThreadedExecutor, the spin() function now will:         - Register timer on Linux;         - Whenever the signal associated with the timer is triggered, the handler will call one run of           schedule();         - schedule() will dispatch callbacks, if any, to available (idle) threads. If no thread is idle, create           a new thread and run the callback in that thread. 2. Added key util functionalities:         - AtomicStack: a lock-free implementation of a stack to store relevant thread data;         - Condition: a light-weight implementation of conditionally wake up/suspend a thread, using futex.
    >Known issue: this implementation is yet to handle mutually exclusive callback groups, which are the default.
    This should be resolved in future commits.

--- rclcpp/src/rclcpp/executors/single_threaded_executor.cpp
+++ rclcpp/src/rclcpp/executors/single_threaded_executor.cpp
@@ -14,10 +14,65 @@
 
 #include "rcpputils/scope_exit.hpp"
 
+#include <pthread.h>
+#include <stdio.h>
+
 #include "rclcpp/executors/single_threaded_executor.hpp"
 #include "rclcpp/any_executable.hpp"
 
+#define UNUSED(expr) do { (void)(expr); } while (0)
+
 using rclcpp::executors::SingleThreadedExecutor;
+
+
+struct t_eventData {
+    syncutil::Condition* signal_scheduler_ptr;
+};
+
+void handler(int sig, siginfo_t *si, void *uc) {
+  UNUSED(sig);
+  UNUSED(uc);
+  t_eventData *data = (t_eventData *) si->_sifields._rt.si_sigval.sival_ptr;
+  data->signal_scheduler_ptr->set_val(1, true);
+}
+
+void* thread_start(void* pthread_arg) {
+  printf("thread start!");
+  rclcpp::executors::PthreadArg* arg = (rclcpp::executors::PthreadArg*) pthread_arg;
+  auto executor = arg->executor;
+  rclcpp::executors::ThreadData thread_data;
+  thread_data.any_exec = arg->any_exec;
+  thread_data.is_busy.set_val(1, false);
+  delete arg;
+  while (true) {
+    thread_data.is_busy.wait_on(0);
+    executor->execute_executable(thread_data.any_exec);
+    thread_data.is_busy.set_val(0, false);
+    executor->idle_threads.push(&thread_data);
+  }
+}
+
+inline void SingleThreadedExecutor::create_thread(rclcpp::AnyExecutable any_exec) {
+    PthreadArg* pthread_arg= new PthreadArg(this, any_exec);
+    pthread_t pid;
+    pthread_create(&pid, NULL, thread_start, (void*) pthread_arg);
+    printf("thread created!\n");
+    pthread_detach(pid);
+    printf("thread detached!\n");
+}
+
+
+
+void SingleThreadedExecutor::assign_or_create(AnyExecutable any_exec) {
+  auto idle_thread = idle_threads.pop();
+  if (idle_thread == nullptr) {
+    create_thread(any_exec);
+    return;
+  }
+  idle_thread->any_exec = any_exec;
+  idle_thread->is_busy.set_val(1, true);
+}
+
 
 SingleThreadedExecutor::SingleThreadedExecutor(const rclcpp::ExecutorOptions & options)
 : rclcpp::Executor(options) {}
@@ -30,11 +85,66 @@ SingleThreadedExecutor::spin()
   if (spinning.exchange(true)) {
     throw std::runtime_error("spin() called while already spinning");
   }
-  RCPPUTILS_SCOPE_EXIT(this->spinning.store(false); );
-  while (rclcpp::ok(this->context_) && spinning.load()) {
-    rclcpp::AnyExecutable any_executable;
-    if (get_next_executable(any_executable)) {
-      execute_any_executable(any_executable);
-    }
+  RCPPUTILS_SCOPE_EXIT(wait_result_.reset();this->spinning.store(false););
+
+  // Clear any previous result and rebuild the waitset
+  this->wait_result_.reset();
+  this->entities_need_rebuild_ = true;
+  timer_t timerId = 0;
+  t_eventData eventData = {&signal_scheduler};
+  union sigval sigv; 
+  sigv.sival_ptr = &eventData;
+  struct sigevent sev = {};
+  sev.sigev_notify = SIGEV_SIGNAL;
+  sev.sigev_signo = SIGRTMIN;
+  sev.sigev_value = sigv;
+  /* specifies the action when receiving a signal */
+  struct sigaction sa = {};
+
+  /* specify start delay and interval */
+  struct itimerspec its = {};
+  struct timespec it_interval = {};
+  struct timespec it_value = {};
+  it_interval.tv_nsec = 50'000'000;
+  it_value.tv_nsec = 50'000'000;
+  its.it_interval = it_interval;
+  its.it_value = it_value;                        
+
+  printf("Signal Interrupt Timer - thread-id: %d\n", gettid());
+
+  /* create timer */
+  if (timer_create(CLOCK_MONOTONIC, &sev, &timerId)){
+      return;
   }
+
+  sa.sa_flags = SA_SIGINFO;
+  sa.sa_sigaction = handler;
+
+  /* Initialize signal */
+  sigemptyset(&sa.sa_mask);
+
+  printf("Establishing handler for signal %d\n", SIGRTMIN);
+
+  /* Register signal handler */
+  if (sigaction(SIGRTMIN, &sa, NULL)) {
+      return;
+  }
+  printf("starting timer\n");
+  if(timer_settime(timerId, 0, &its, NULL)) {
+      return;
+  }
+  while (rclcpp::ok(this->context_) && spinning.load()) {
+    this->schedule();
+  }
+}
+
+void SingleThreadedExecutor::schedule() {
+  this->signal_scheduler.wait_on(0);
+  rclcpp::AnyExecutable executable;
+  while (get_next_executable(executable)) {
+    printf("Executable available, assigning to threads.\n");
+    assign_or_create(executable);
+  }
+  printf("No more available executable.\n");
+  this->signal_scheduler.set_val(0, false);
 }
