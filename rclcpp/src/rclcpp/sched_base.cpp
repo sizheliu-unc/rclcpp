9760f470cb49b820ba783f9edaa3152ab221920a
Rohan Wagle <wagle@cs.unc.edu>
2025-02-14 04:13:48 -0500

    various changes while testing

--- rclcpp/src/rclcpp/sched_base.cpp
+++ rclcpp/src/rclcpp/sched_base.cpp
@@ -5,6 +5,7 @@ namespace sched {
 int PureEDF::pure_edf_fd = -1;
 bool update_deadline(pthread_t pthread_id, PureEDF* edf_attr) {
     if (PureEDF::pure_edf_fd < 0) {
+        std::cout << "Pure EDF fd is invalid!" << std::endl;
         return false;
     }
     sched_param ext_param = {0};
@@ -12,6 +13,7 @@ bool update_deadline(pthread_t pthread_id, PureEDF* edf_attr) {
     attr.pid = get_pid(pthread_id);
     attr.abs_deadline = edf_attr->abs_deadline;
     ssize_t bytes_written = write(PureEDF::pure_edf_fd, &attr, sizeof(EDF_attr_struct));
+    std::cout << "Wrote " << bytes_written << " bytes to pipe on fd " << PureEDF::pure_edf_fd << std::endl;
     pthread_setschedparam(pthread_id, 7, &ext_param);
     return bytes_written > 0;
 }
