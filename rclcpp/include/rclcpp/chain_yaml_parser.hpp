a568d50303b9fdf3e531ae8f4c07c7aa1e9d0007
Rocksen1683 <edpravee@uwaterloo.ca>
2026-01-19 18:04:01 -0500

    migrated yaml parser

---
+++ rclcpp/include/rclcpp/chain_yaml_parser.hpp
@@ -0,0 +1,63 @@
+// Copyright 2025 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef RCLCPP__CHAIN_YAML_PARSER_HPP_
+#define RCLCPP__CHAIN_YAML_PARSER_HPP_
+
+#include <yaml-cpp/yaml.h>
+
+#include <cstdint>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "rclcpp/visibility_control.hpp"
+
+namespace rclcpp
+{
+
+struct UserChain
+{
+  std::string chain_name;
+  std::vector<std::string> callbacks;
+  std::uint32_t deadline;
+  std::uint32_t period;
+};
+
+class RCLCPP_PUBLIC ChainYamlParser
+{
+public:
+  ChainYamlParser() = default;
+  ~ChainYamlParser() = default;
+
+  void load_yaml_file(const std::string & yaml_file);
+  bool parse();
+  const std::unordered_map<std::string, UserChain> & get_user_chains() const
+  {
+    return user_chains_;
+  }
+
+private:
+  static constexpr const char * kCallbacksKey = "callbacks";
+  static constexpr const char * kDeadlineKey = "deadline";
+  static constexpr const char * kPeriodKey = "period";
+
+  std::string yaml_file_;
+  YAML::Node yaml_node_;
+  std::unordered_map<std::string, UserChain> user_chains_;
+};
+
+}  // namespace rclcpp
+
+#endif  // RCLCPP__CHAIN_YAML_PARSER_HPP_
